c34a3d384f5f5727a919d1611af6605344454ef9
Dyberg <mathias.rerup-dyberg@hotmail.com>
2024-04-23 09:56:58 +0200

    Init, belief base made

---
+++ AiAssignment2/Engine.cs
@@ -0,0 +1,21 @@
+namespace AiAssignment2;
+
+public class Engine
+{
+    public HashSet<string> BeliefBase;
+
+    public Engine()
+    {
+        BeliefBase = new HashSet<string>();
+    }
+
+    public void Resolve(string belief1, string belief2)
+    {
+        
+    }
+
+    public void Contract(string belief)
+    {
+        
+    }
+}
--- AiAssignment2/Program.cs
+++ AiAssignment2/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AiAssignment2;
 
 Console.WriteLine("Hello, World!");
+
+//Forestil dig du starter med og tro på noget som, jorden er flad. Så får du at vide at hvis jorden er flad,
+//så kan du se bjerge fra dit vindue. Du lærer så senere at man ikke kan se bjerge fra dit vindue, så nu skal vi fjerne
+//den fra vores belief base, og opdatere de andre ting der er relateret til vores nye information. 
+
+
+//design and implementation of belief base
+//Måske bruge et hashtable eller hashset
+
+//Når man får ny information, skal man gennemgå alt information vi allerede kender
+
+var engine = new Engine();
+
+engine.BeliefBase.Add("A");
+engine.BeliefBase.Add("B");
+if (engine.BeliefBase.Contains("A"))
+{
+    engine.BeliefBase.Remove("A");
+}
+
+foreach (var belief in engine.BeliefBase)
+{
+    Console.WriteLine(belief);
+}
+
+
+
+
+
+
+
+
