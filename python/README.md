b87e8cc2518639915e739fe5769db512c45325b9
Gideon <gidewadejob@gmail.com>
2024-04-28 20:20:50 +0100

    lets go final WWWW

---
+++ python/README.md
@@ -0,0 +1,9 @@
+# Belief Revision Assignment
+Welcome to our version of Belief Revision, made with help from ``sumpy``
+
+## Run
+To run the program, its straight forward, you simply run the main.py file, can be done with the commando 
+``python main.py``
+
+## Edit belief base
+To edit the belief base head to the buttom of the main.py file, here you will find a function called "make_belief_base" simply update this function with the new belief base. 
--- python/main.py
+++ python/main.py
@@ -1,20 +1,9 @@
-from sympy.logic.boolalg import to_cnf, eliminate_implications
+from sympy.logic.boolalg import to_cnf
+from sympy.logic.inference import satisfiable
 from sympy import symbols, And, Or, Not, Implies
 from sympy.abc import A, B, D, R, P, S
-from itertools import product
 
-
-
-class Symbol:
-    def __init__(self, name : str, value : bool) -> None:
-        self.name : str = name
-        self.value : bool = value
-
-def make_belief_base():
-    A, B, D = symbols('A B D')
-    belief_base = (Implies(~B , D) & (~A | B)) & ~D
-    belief_base_cnf = to_cnf(belief_base)
-    return belief_base_cnf
+############## The belief revision agent ##############
 
 def make_clauses(belief_base_cnf):
     listed_beliefs_clauses = []
@@ -27,7 +16,7 @@ def make_clauses(belief_base_cnf):
     clauses = []
     for clause in listed_beliefs_clauses:
         if isinstance(clause, Or):
-            clauses.append(set(clause.args))              
+            clauses.append(set(clause.args))
         else:
             new_set = set()
             new_set.add(clause)   
@@ -36,34 +25,9 @@ def make_clauses(belief_base_cnf):
 
 def entails(belief_base_cnf, proposition) -> list[set]:
     clauses = make_clauses(expansion(belief_base_cnf, Not(proposition)))
-    print("CLAUSES FINAL: " + str(clauses))
-    # Code above makes belief_base into list of lists representing clauses  [{~D}, {B, D}, {B, ~A}, {~B}]
-    # Example 
-    #                         Belief base                           |                 Proposition
-    # Original  |    B & (Implies(~B , D) & (~A | B)) & ~D          |                Implies(B, ~A)
-    # CNF       |    B & ~D & (B | D) & (B | ~A)                    |                    A | B
-    # Clausal   |    [[B], [~D], [B, D], [B, ~A],                   |                   [A, B]]           
-    return resolution2(clauses)
+    return resolution(clauses)
 
 def resolution(clauses):
-    resolution_clauses = []
-    for clause1 in clauses:
-        current_clauses = []
-        for symbol in clause1:
-            add = True
-            for clause2 in clauses:
-                if Not(symbol) in clause2:
-                    clause2.remove(Not(symbol))
-                    add = False
-            if add:
-                current_clauses.append(symbol)
-        resolution_clauses.append(current_clauses)
-    # Code above compares the clauses and removes symbols if they intertwine. 
-    # Code below checks if an empty claus is present implying 
-    # that the belief base entails the proposition.
-    return [] in resolution_clauses
-
-def resolution2(clauses):       #[{B}, {~D}, {D, B}, {~A, B}, {~A}, {~D}]
     for clause1 in clauses:
         for element in clause1:
             for clause2 in clauses:
@@ -76,19 +40,18 @@ def resolution2(clauses):       #[{B}, {~D}, {D, B}, {~A, B}, {~A}, {~D}]
                     if len(new_clause) == 0:
                         return True
                     elif new_clause not in clauses:
-                        return resolution2(clauses + [new_clause])
+                        return resolution(clauses + [new_clause])
     return False
 
 def contraction(belief_base_cnf, proposition):
-    clauses = make_clauses(belief_base_cnf)
-    for clause in clauses:
-        if proposition in clause:
-            clause.remove(proposition)
-    no_empty_clauses = []
-    for clause in clauses:
-        if len(clause) != 0:
-            no_empty_clauses.append(clause)
-    return from_clause_to_belief_base(no_empty_clauses)
+    bb_clauses = make_clauses(belief_base_cnf)
+    prop_clauses = make_clauses(to_cnf(proposition))
+    clauses_new = bb_clauses.copy()
+    for prop_clause in prop_clauses:
+        for bb_clause in bb_clauses:
+            if prop_clause == bb_clause:
+                clauses_new.remove(bb_clause)
+    return from_clause_to_belief_base(clauses_new)
 
 def from_clause_to_belief_base(clauses):
     result = [] 
@@ -96,42 +59,92 @@ def from_clause_to_belief_base(clauses):
         if len(clause) > 1:
             result.append(Or(*clause))
         else:
-            result.append(clause[0])
+            result.append(And(*clause))
     belief_base_cnf = to_cnf(And(*result))
     return belief_base_cnf
 
 def expansion(belief_base_cnf, proposition):
     return to_cnf(belief_base_cnf & proposition)
 
+def revision(belief_base_cnf, proposition):
+    #Levi identity
+    return expansion(contraction(belief_base_cnf, Not(proposition)), proposition)
 
-belief_base_cnf = make_belief_base()
-proposition = ~D
-print(f"{belief_base_cnf} entails {proposition}")
-print(entails(belief_base_cnf, proposition))
+############## BELOW ARE AGM POSTULATES ##############
 
-print(f"before contraction/expansion  {belief_base_cnf}")
-belief_base_cnf = contraction(belief_base_cnf, proposition)
-print(f"contraction                   {belief_base_cnf}")
-belief_base_cnf = expansion(belief_base_cnf, proposition)
-print(f"expansion                     {belief_base_cnf}")
+def success(belief_base_cnf, proposition):
+    revised_belief = revision(belief_base_cnf,proposition)
 
+    prop_cnf = make_clauses(to_cnf(proposition))
+    revised_belief = make_clauses(revised_belief)
+
+    for element in prop_cnf:
+        if not element in revised_belief:
+            return False
+    return True
+
+def inclusion(belief_base_cnf, proposition):
+    revised_belief = revision(belief_base_cnf, proposition)
+    expanded_belief = expansion(belief_base_cnf, proposition)
+
+    revised_belief = make_clauses(revised_belief)
+    expanded_belief = make_clauses(expanded_belief)
+    
+    for element in revised_belief:
+        if element not in expanded_belief:
+            return False
+    return True
+
+def vacuity(belief_base_cnf, proposition):
+    if contraction(belief_base_cnf, Not(proposition)) == belief_base_cnf:
+        return revision(belief_base_cnf, proposition) == expansion(belief_base_cnf, proposition)
+    return f"negated proposition: {Not(proposition)} was a part of belief_base_cnf: {belief_base_cnf}"
+
+
+def consistency(belief_base_cnf, proposition):
+    #The sympy function satisfiable is a check for whether a belief is consistent
+    if satisfiable(to_cnf(proposition)):
+        return satisfiable(revision(belief_base_cnf,proposition))
+    else:
+        return True
+
+
+def extensionality(belief_base_cnf, proposition1, proposition2):
+    p1_cnf = to_cnf(proposition1)
+    p2_cnf = to_cnf(proposition2)
+    if p1_cnf == p2_cnf:
+        return revision(belief_base_cnf, p1_cnf) == revision(belief_base_cnf, p2_cnf)
+    return f"proposition 1: {p1_cnf} and proposition 2: {p2_cnf} are not logically equivalent"
+
+
+############## AGM POSTULATES END ##############
+
+def make_belief_base():
+    A, B, D = symbols('A B D')
+    belief_base = (Implies(~B , D) & (~A | B)) & ~D & (~R | P | S)
+    belief_base_cnf = to_cnf(belief_base)
+    return belief_base_cnf
+
+def main():
+    belief_base_cnf = make_belief_base()
+    proposition = ~D
+    print(f"{belief_base_cnf} entails {proposition}")
+    print(entails(belief_base_cnf, proposition))
+
+    print(f"before contraction/expansion  {belief_base_cnf}")
+    belief_base_cnf = contraction(belief_base_cnf, proposition)
+    print(f"contraction                   {belief_base_cnf}")
+    belief_base_cnf = expansion(belief_base_cnf, proposition)
+    print(f"expansion                     {belief_base_cnf}")
+
+    print("AGM now")
+    print(vacuity(belief_base_cnf, proposition))
+    proposition2 = B | A
+    print(extensionality(belief_base_cnf, proposition, proposition2))
+
+main()
 
 # & = AND
 # | = OR
 # ~ = NOT
 # >> = IMPLIES1
-
-
-# 1. design and implementation of belief base; W
-
-# 2. design and implementation of a method for checking logical entailment (e.g., resolution-
-# based), you should implement it yourself, without using any existing packages; W
-
-# 3. implementation of contraction of belief base (based on a priority order on formulas in the
-# belief base);
-
-# 4. implementation of expansion of belief base. W
-
-# The output should be the resulting/new belief base.
-
-
